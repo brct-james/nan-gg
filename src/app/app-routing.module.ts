a43dca3e298d9f3c4c3c00aec20148c524044105
James C <james.clague@brct.io>
2021-11-30 20:22:50 +0000

    Routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,20 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomepageComponent } from './homepage/homepage.component';
 import { AccountsComponent } from './accounts/accounts.component';
+import { GoalsComponent } from './goals/goals.component';
+import { BudgetsComponent } from './budgets/budgets.component';
+import { FundsComponent } from './funds/funds.component';
+import { TransactionsComponent } from './transactions/transactions.component';
+import { SettingsComponent } from './settings/settings.component';
 
 const routes: Routes = [
   { path: '', component: HomepageComponent },
   { path: 'a', component: AccountsComponent },
+  { path: 'g', component: GoalsComponent },
+  { path: 'b', component: BudgetsComponent },
+  { path: 'f', component: FundsComponent },
+  { path: 't', component: TransactionsComponent },
+  { path: 'settings', component: SettingsComponent },
 ];
 
 @NgModule({
