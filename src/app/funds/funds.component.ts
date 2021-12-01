1847fe2041798aa38b318752f8ca10778924f2a2
James C <james.clague@brct.io>
2021-12-01 17:09:31 +0000

    Basic funds

--- src/app/funds/funds.component.ts
+++ src/app/funds/funds.component.ts
@@ -3,13 +3,39 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-funds',
   templateUrl: './funds.component.html',
-  styleUrls: ['./funds.component.sass']
+  styleUrls: ['./funds.component.sass'],
 })
 export class FundsComponent implements OnInit {
+  constructor() {}
+  fundsArr: any[] = [
+    {
+      fuid: 0,
+      name: 'Pet Food',
+      description: 'Food for pets',
+      category: 'Pets',
+      fund_number: 'PETS-0',
+      account: 'Chase JC',
+      balance: 100,
+    },
+    {
+      fuid: 1,
+      name: 'Pet Meds',
+      description: 'Elvis Inhaler',
+      category: 'Pets',
+      fund_number: 'PETS-1',
+      account: 'Chase JC',
+      balance: 200,
+    },
+    {
+      fuid: 2,
+      name: 'Groceries',
+      description: 'Human Food',
+      category: 'Living Expenses',
+      fund_number: 'LIVEX-0',
+      account: 'Chase JC',
+      balance: 375,
+    },
+  ];
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
