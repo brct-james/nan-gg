2c20fcd828aaf12909805100e3b07234af5c36ec
James C <james.clague@brct.io>
2021-12-01 17:02:44 +0000

    Basic budgets

--- src/app/budgets/budgets.component.ts
+++ src/app/budgets/budgets.component.ts
@@ -3,13 +3,39 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-budgets',
   templateUrl: './budgets.component.html',
-  styleUrls: ['./budgets.component.sass']
+  styleUrls: ['./budgets.component.sass'],
 })
 export class BudgetsComponent implements OnInit {
+  constructor() {}
+  budgetsArr: any[] = [
+    {
+      month: 'December',
+      year: 2021,
+      name: 'December Budget',
+      description: '',
+      income: 2000,
+      attributed: 1500,
+      balance: 1000,
+    },
+    {
+      month: 'January',
+      year: 2022,
+      name: 'January Budget',
+      description: '',
+      income: 2000,
+      attributed: 1500,
+      balance: 1000,
+    },
+    {
+      month: 'February',
+      year: 2022,
+      name: 'February Budget',
+      description: '',
+      income: 2000,
+      attributed: 1500,
+      balance: 1000,
+    },
+  ];
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
